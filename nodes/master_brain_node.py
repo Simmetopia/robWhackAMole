7412f1784ecdbc96a291f55e55213cd047730b52
Luise <luise.sorensen@gmail.com>
2019-01-04 03:17:38 -0800

    implements take_image and adjust function names

--- nodes/master_brain_node.py
+++ nodes/master_brain_node.py
@@ -10,6 +10,7 @@ from configuration_loader import ConfigurationLoader
 
 
 class MasterBrainNode:
+    
     def __init__(self, target_topic, request_topic):
         self.target_publisher = rospy.Publisher(target_topic, Targets, queue_size=10)
         rospy.Subscriber(request_topic, Empty, self._get_vision_data)
@@ -17,8 +18,11 @@ class MasterBrainNode:
 
     def _get_vision_data(self, empty):
         # get objects on table and game mode
-        self._take_image()
-        self._split_image()
+        image = self._take_image()
+        
+        #vision.FindObjects(image)
+        
+        self._filter_objects()
         # interpret game mode and select targets
         # convert target cords to robot target cords
         convert()
@@ -28,9 +32,31 @@ class MasterBrainNode:
         self.target_publisher.publish(targets)
 
     def _take_image(self):
-        raise NotImplementedError
+        """
+        Fetches an image from the webcam
+        """
+        print "try fetch from webcam..."
+        stream = urllib.urlopen('http://192.168.0.20/image/jpeg.cgi')
+        bytes = ''
+        bytes += stream.read(64500)
+        a = bytes.find('\xff\xd8')
+        b = bytes.find('\xff\xd9')
+    
+        if a != -1 and b != -1:
+            jpg = bytes[a:b + 2]
+            bytes = bytes[b + 2:]
+            i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),
+                             cv2.CV_LOAD_IMAGE_COLOR)
+            return i
+        else:
+            print "did not receive image, "
+            print "try increasing the buffer size in line 13:"
 
-    def _split_image(self):
+
+    def _filter_object(self):
+        raise NotImplementedError
+    
+    def _find_mode(self):
         raise NotImplementedError
 
 
